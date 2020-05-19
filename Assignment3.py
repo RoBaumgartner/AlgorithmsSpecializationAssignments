4bfa139ce9f7a7aa90a1c453e6ad72ab5652a71d
Robert Baumgartner <baumgar.robert@gmail.com>
2020-05-19 12:55:38 +0200

    Did assignment 3

--- Assignment3.py
+++ Assignment3.py
@@ -1,6 +1,7 @@
 """Implement quicksort with 3 different policies for chosing pivot element. Assumption: input has only distict elements"""
 
 from enum import Enum
+from math import floor
 
 class Question(Enum):
   Q1 = 1
@@ -12,15 +13,13 @@ q = Question.Q3
 
 def choosePivot(array, l, r): # 
   if q == Question.Q1:
-    return l
+    return
   elif q == Question.Q2:
     elem = array[l]
     array[l] = array[r]
     array[r] = elem
-    return l
   else:
-    n = len(array)
-    m = [int(n / 2) if n % 2 != 0 else int((n / 2) - 1)][0]
+    m = floor((l + r) / 2)
     
     values = [array[l], array[m], array[r]]
     indices = [l, m, r]
@@ -31,40 +30,36 @@ def choosePivot(array, l, r): #
 
     iMax = values.index(max(values))
     elem = array[l]
-    array[l] = array[iMax]
-    array[iMax] = elem
+    array[l] = array[indices[iMax]]
+    array[indices[iMax]] = elem
 
-    return l
-
-def quickSort(array, l, r, n):
+def quickSort(array, l, r):
   if l >= r:
     return 0
 
-  p = choosePivot(array, l, r)
+  choosePivot(array, l, r)
  
-  # put p in rightous place
-  i = l
-  for j in range(i + 1, r + 1):
-    if i == p:
-      i += 1
-    if array[j] < array[p]: # swap i and j
+  # put pivot in rightous place
+  i = l + 1
+  for j in range(i, r + 1):
+    if array[j] < array[l]:
       elem = array[j]
       array[j] = array[i]
       array[i] = elem
       i += 1
   elem = array[i - 1]
-  array[i - 1] = array[p]
-  array[p] = elem
+  array[i - 1] = array[l]
+  array[l] = elem
 
   p = i - 1
-  n += quickSort(array, l, p - 1, n)
-  n += quickSort(array, p + 1, r, n)
+  nLeft = quickSort(array, l, p - 1)
+  nRight = quickSort(array, p + 1, r)
 
-  return n + 1
+  return nLeft + nRight + len(array[l:r+1]) - 1
 
 if __name__ == "__main__":
-  file = open("Test1.txt", "r") # Assignment3_Input
+  file = open("Assignment3_Input.txt", "r") # Assignment3_Input
   array = [int(i) for i in file]
 
-  n = quickSort(array, 0, len(array) - 1, 0)
-  print(array)
+  n = quickSort(array, 0, len(array) - 1)
+  print(n)
